9a23e64bab2d32687a4f0a09927ee77e23281679
shouta-nakashima <fantajista5.6nakaji.s@gmail.com>
2023-07-07 20:16:01 +0900

    feat: supabase との連携と segments の設定

---
+++ apps/web/src/components/NavBar.tsx
@@ -0,0 +1,36 @@
+import Link from 'next/link';
+
+const NavBar = () => {
+  return (
+    <header className='bg-gray-800 p-4'>
+      <nav className='space-x-4'>
+        <Link
+          href='/'
+          className='rounded bg-gray-700 px-3 py-2 text-white hover:bg-gray-500'
+        >
+          Home
+        </Link>
+        <Link
+          href='/blogs'
+          className='rounded bg-gray-700 px-3 py-2 text-white hover:bg-gray-500'
+        >
+          Blogs
+        </Link>
+        <Link
+          href='/streaming-sr'
+          className='rounded bg-gray-700 px-3 py-2 text-white hover:bg-gray-500'
+        >
+          Streaming SR
+        </Link>
+        <Link
+          href='/auth'
+          className='rounded bg-gray-700 px-3 py-2 text-white hover:bg-gray-500'
+        >
+          Auth with CRUD
+        </Link>
+      </nav>
+    </header>
+  )
+}
+
+export default NavBar
